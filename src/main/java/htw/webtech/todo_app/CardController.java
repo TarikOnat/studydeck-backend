7bb4b863a6e641eb3930306e901d8e2cc1b69725
TarikOnat <tarik151@gmx.de>
2025-11-03 20:06:37 +0100

    Backend: Card-Klasse hinzugefügt und /api/cards Endpoint erstellt (inkl. CORS für Frontend)

--- src/main/java/htw/webtech/todo_app/CardController.java
+++ src/main/java/htw/webtech/todo_app/CardController.java
@@ -3,7 +3,9 @@ package htw.webtech.todo_app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class CardController {
 
@@ -11,7 +13,8 @@ public class CardController {
     public List<Card> getAllCards() {
         return List.of(
                 new Card(1L, "Was ist 5x5?", "25"),
-                new Card(2L, "Was ist die Hauptstadt von Deutschland?", "Berlin")
+                new Card(2L, "Was ist die Hauptstadt von Deutschland?", "Berlin"),
+                new Card(3L, "Wofür steht HTW?", "Hochschule für Technik und Wirtschaft")
         );
     }
 }
